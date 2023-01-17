534b597dc8478efc13b8f1bd024ce958f7695386
BryanCabrera09 <81339678+BryanCabrera09@users.noreply.github.com>
2023-01-16 21:26:14 -0500

    first commit

---
+++ src/java/modelo/Empleado.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author BRYAN_CABRERA
+ */
+@Entity
+@Table(name = "empleado")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Empleado.findAll", query = "SELECT e FROM Empleado e")
+    , @NamedQuery(name = "Empleado.findByIdEmpleado", query = "SELECT e FROM Empleado e WHERE e.idEmpleado = e.idEmpleado")
+    , @NamedQuery(name = "Empleado.findByNombreEmp", query = "SELECT e FROM Empleado e WHERE e.nombreEmp = e.nombreEmp")})
+public class Empleado implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id_empleado")
+    private Integer idEmpleado;
+    /*@Basic(optional = false)
+    @NotNull*/
+    @Column(name = "nombre_emp", length = 50)
+    private String nombreEmp;
+    @JoinColumn(name = "id_departamento", referencedColumnName = "id_departamento")
+    @ManyToOne(optional = false)
+    private Departamento idDepartamento;
+
+    public Empleado() {
+    }
+
+    public Empleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public Empleado(Integer idEmpleado, String nombreEmp) {
+        this.idEmpleado = idEmpleado;
+        this.nombreEmp = nombreEmp;
+    }
+
+    public Integer getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(Integer idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombreEmp() {
+        return nombreEmp;
+    }
+
+    public void setNombreEmp(String nombreEmp) {
+        this.nombreEmp = nombreEmp;
+    }
+
+    public Departamento getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(Departamento idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEmpleado != null ? idEmpleado.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Empleado)) {
+            return false;
+        }
+        Empleado other = (Empleado) object;
+        if ((this.idEmpleado == null && other.idEmpleado != null) || (this.idEmpleado != null && !this.idEmpleado.equals(other.idEmpleado))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Empleado[ idEmpleado=" + idEmpleado + " ]";
+    }
+    
+}
