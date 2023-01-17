341db5879140b8c157e694d65ebbb9cc1db21434
BryanCabrera09 <81339678+BryanCabrera09@users.noreply.github.com>
2023-01-16 22:28:21 -0500

    first commit

--- build/generated-sources/ap-source-output/modelo/Departamento_.java
+++ build/generated-sources/ap-source-output/modelo/Departamento_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.Empleado;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-16T19:34:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-16T22:21:33")
 @StaticMetamodel(Departamento.class)
 public class Departamento_ { 
 
--- build/generated-sources/ap-source-output/modelo/Empleado_.java
+++ build/generated-sources/ap-source-output/modelo/Empleado_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.Departamento;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-16T19:34:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-01-16T22:21:33")
 @StaticMetamodel(Empleado.class)
 public class Empleado_ { 
 
